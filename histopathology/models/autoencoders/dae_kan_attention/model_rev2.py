fix: resolve channel mismatches and KAN regularization loss calculation

histopathology/models/autoencoders/dae_kan_attention/model_rev2.py
@@ -50,7 +50,7 @@ class Autoencoder_Encoder(nn.Module):
         )
 
         self.decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(128, 384, kernel_size=3, padding=1, stride=2, output_padding=1),
+            nn.ConvTranspose2d(16, 384, kernel_size=3, padding=1, stride=2, output_padding=1),
             nn.BatchNorm2d(384),
             nn.ELU(inplace=True), 
         )

histopathology/models/components/kan/KANConv.py
@@ -130,7 +130,7 @@ class KAN_Convolution(torch.nn.Module):
         return convolution.kan_conv2d(x, self.conv,self.kernel_size[0],self.stride,self.dilation,self.padding,self.device)
     
     def regularization_loss(self, regularize_activation=1.0, regularize_entropy=1.0):
-        return sum( layer.regularization_loss(regularize_activation, regularize_entropy) for layer in self.layers)
+        return self.conv.regularization_loss(regularize_activation, regularize_entropy)
 
 
 